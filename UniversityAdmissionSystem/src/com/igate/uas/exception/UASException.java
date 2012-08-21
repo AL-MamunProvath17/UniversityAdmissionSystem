03a672db4add9abf990b6c5d7a9fe43f72d9ad8f
Jimish Bhayani <jimishbhayani@gmail.com>
2012-08-21 18:28:01 +0530

    Exception Class Added

---
+++ UniversityAdmissionSystem/src/com/igate/uas/exception/UASException.java
@@ -0,0 +1,21 @@
+package com.igate.uas.exception;
+
+@SuppressWarnings("serial")
+public class UASException extends Exception {
+
+	public UASException(String message) {
+		super(message);
+	}
+	/**
+	 *
+	 * File Name : UASException.java
+	 * Class Name : UASException
+	 * Description : 
+	 * Created On : Aug 21, 2012
+	 * @author : jb804412
+	 *  
+	 */
+	
+	
+
+}
